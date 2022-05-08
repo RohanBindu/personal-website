17fe283f72db25cf25da2a9034049a97f89ab1d2
rohan bindu <rbindu42@gmail.com>
2022-05-07 17:56:49 -0700

    cool music addition

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Route } from "react-router-dom";
 import Main from './components/Main';
 
 
+
 //Components
 import AboutPage from './components/AboutPage';
 import Hoop from './components/Hoop';
@@ -13,6 +14,7 @@ import Projects from './components/Projects';
 import SkillsPage from './components/SkillsPage';
 import { useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import SoundBar from "./subComponents/SoundBar";
 
 
 function App() {
@@ -23,6 +25,7 @@ function App() {
     <GlobalStyle />
 
       <ThemeProvider theme={lightTheme}>
+      <SoundBar />
       
       <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.pathname}>
